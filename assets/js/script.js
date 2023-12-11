b8b90238d13a261eec570c08c10b10deebdcd1f8
Jorritvans <>
2023-12-11 10:48:25 +0000

    adding the canvas setup, ball variables found on google and paddle movement variables which were all taken fron w3schools.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,20 @@
-console.log("Connected!")
+/**
+ *Canvas and Context setup
+ */
+var canvas = document.getElementById("gamecanvas");
+var ctx = canvas.getContext("2d");
+ctx.canvas.width = window.innerWidth - 30;
+ctx.canvas.height = window.innerHeight - 50;
+
+/**Ball variables */
+var ballRadius = 10;
+var x = canvas.width / 2;
+var y = canvas.height - 90;
+var dx = 2;
+var dy = -2;
+var offsetBottom = 30;
+
+/**Paddle & movement variable variables*/
+var rightPressed = false;
+var leftPressed = false;
+var PADDLE_VARS = { height: 10, width: 65, x: (canvas.width - 65) / 2 };
